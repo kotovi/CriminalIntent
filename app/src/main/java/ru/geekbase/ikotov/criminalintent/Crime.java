c991521141369fcbb75bd3501894f052d6a522e1
Ivan Kotov <kotov.irk@gmail.com>
2018-10-31 14:29:57 +0800

    1.0.1

---
+++ app/src/main/java/ru/geekbase/ikotov/criminalintent/Crime.java
@@ -0,0 +1,16 @@
+package ru.geekbase.ikotov.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSloved;
+    public Crime(){
+        mId = UUID.randomUUID();
+        mDate = new
+    }
+
+}
