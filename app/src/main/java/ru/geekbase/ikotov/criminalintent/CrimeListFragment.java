39074334d82ad104e955a452b7b264c6a39ab8ad
i.kotov_home <gardener@iMac-Gardener.local>
2018-11-24 11:21:58 +0800

    1.0.9
    page 198

--- app/src/main/java/ru/geekbase/ikotov/criminalintent/CrimeListFragment.java
+++ app/src/main/java/ru/geekbase/ikotov/criminalintent/CrimeListFragment.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -29,16 +30,25 @@ public class CrimeListFragment extends Fragment {
         return view;
     }
 
-    private class CrimeHolder extends RecyclerView.ViewHolder {
+    private class CrimeHolder extends RecyclerView.ViewHolder  implements View.OnClickListener {
         public CrimeHolder(LayoutInflater inflater, ViewGroup paren){
             super(inflater.inflate(R.layout.list_item_crime, paren, false));
             mTitleTextView = (TextView) itemView.findViewById(R.id.crime_title);
             mDateTextView = (TextView) itemView.findViewById(R.id.crime_date);
+
+            itemView.setOnClickListener(this);
         }
         public void bind (Crime crime){
             mCrime = crime;
             mTitleTextView.setText(mCrime.getTitle());
             mTitleTextView.setText(mCrime.getDate().toString());
+        }
+        @Override
+        public void onClick(View view) {
+
+            Toast.makeText(getActivity(), mCrime.getTitle()+ " clicked!", Toast.LENGTH_SHORT)
+                    .show();
+
         }
     }
 
